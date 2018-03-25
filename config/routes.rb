31e1c7567bfeeccdd1395e1cd6f22819560954ec
LinnierGames <esericksanc@gmail.com>
2018-03-25 16:21:32 -0700

    Add Site to inherit from Hote
    Due note this about STI, or single table inheritance:
    Be aware that because the type column is an attribute on the record every new subclass will instantly be marked as dirty and the type column will be included in the list of changed attributes on the record. More on this here http://api.rubyonrails.org/classes/ActiveRecord/Inheritance.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-
   resources :users, expect: [:create] do
     resources :trips do
       resources :hotels
+      resources :sites
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325211413) do
+ActiveRecord::Schema.define(version: 20180325231353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180325211413) do
     t.integer "ratings"
     t.bigint "trip_id"
     t.bigint "user_id"
+    t.string "type"
     t.index ["trip_id"], name: "index_hotels_on_trip_id"
     t.index ["user_id"], name: "index_hotels_on_user_id"
   end
