edf71b90ae79a3f494b5e5c94a9ee89bf7abcce8
LinnierGames <esericksanc@gmail.com>
2018-04-02 15:55:10 -0700

    Add Cover Image Url to Trips

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,6 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database.
+# incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326000222) do
+ActiveRecord::Schema.define(version: 20180402224847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180326000222) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.boolean "is_public"
+    t.string "cover_image_url"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
