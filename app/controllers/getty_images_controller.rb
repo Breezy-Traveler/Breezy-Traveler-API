19017ffece138310bcdfeb3ef97597e925f72932
LinnierGames <esericksanc@gmail.com>
2018-04-02 15:29:26 -0700

    Map Response to only list of links
    WIP: remove the 'root->images' to return an array of the links

--- app/controllers/getty_images_controller.rb
+++ app/controllers/getty_images_controller.rb
@@ -1,17 +1,30 @@
 require 'http'
+require 'json'
+
+class GettyImagesSerializer < ActiveModel::Serializer
+  attributes :images
+
+  def images
+    JSON.parse(object.body.to_s)["images"].map do |an_image|
+      an_image["display_sizes"][0]["uri"]
+    end
+  end
+end
+
+
 class GettyImagesController < ApplicationController
   def index
     term = params[:search_term]
 
     if term
-      response = HTTP["Api-Key" => ENV["GITTY-APP-KEY"]]
+      response = HTTP["Api-Key" => ENV["GETTY-APP-KEY"]]
                      .get("https://api.gettyimages.com/v3/search/images",
                           :params => {
                               :fields => "thumb",
                               :phrase => term
                           })
 
-      render json: response.body
+      render json: response, serializer: GettyImagesSerializer, root: "images"
     end
   end
 end
