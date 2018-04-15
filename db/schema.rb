2db8c37eed176fe33c44e31513af0aa59d93cee2
LinnierGames <esericksanc@gmail.com>
2018-04-15 16:56:31 -0700

    Use Datetime for Trips
    Swift sends dates with time and the presistance store rejects this value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407032742) do
+ActiveRecord::Schema.define(version: 20180415234912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,14 +28,14 @@ ActiveRecord::Schema.define(version: 20180407032742) do
 
   create_table "trips", force: :cascade do |t|
     t.string "place"
-    t.date "start_date"
-    t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.boolean "is_public"
     t.string "cover_image_url"
     t.string "notes"
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
