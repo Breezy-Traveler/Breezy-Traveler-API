b6973b4f669e428339490c0b50442288cb418391
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-23 15:15:51 -0700

    removed all rails files

--- app/models/trip.rb
+++
@@ -1,9 +0,0 @@
-class Trip < ApplicationRecord
-  validates_presence_of :place
-
-  validates_inclusion_of :is_public, :in => [true, false]
-
-  has_many :hotels, -> { where type: nil }
-  has_many :sites, -> { where type: "Site" }
-  belongs_to :user
-end
--- db/migrate/20180415234912_update_date_type_for_trips.rb
+++
@@ -1,9 +0,0 @@
-class UpdateDateTypeForTrips < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :trips, :start_date, :date
-    remove_column :trips, :end_date, :date
-
-    add_column :trips, :start_date, :datetime
-    add_column :trips, :end_date, :datetime
-  end
-end
