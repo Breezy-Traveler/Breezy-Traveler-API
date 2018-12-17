9e46f2cb1fce9f01738a1c188b00e77fa9a4bdb7
Phyllis Wong <wong.phyllism@gmail.com>
2018-12-17 11:33:14 -0800

    closes #92 removed all flash messaging from application

--- src/config/passport.js
+++ src/config/passport.js
@@ -62,13 +62,11 @@ module.exports = function(passport) {
 
         // if no user is found, return the message
         if (!user)
-          // req.flash is the way to set flashdata using connect-flash
-          return done(null, false, req.flash('loginMessage', 'No user found.'));
+          return done(null, false);
 
         // if the user is found but the password is wrong
         if (!user.validPassword(password))
-          // create the loginMessage and save it to session as flashdata
-          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+          return done(null, false);
 
         // all is well, return successful user
         return done(null, user);
